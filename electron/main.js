ee45e4e4457d28255bbb805e96be3c555d70e513
Abhimanyu Kumar <akabhimanyukumar111@gmail.com>
2024-11-13 12:13:20 +0530

    Initial Commit

---
+++ electron/main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: false,
+            nodeIntegration: true,
+        },
+    });
+
+    // Load your Next.js app (localhost during development)
+    win.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
